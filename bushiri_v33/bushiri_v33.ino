4a05afc07eab2588c0c25ac6a55bf460547519a2
bushiriproject <bushiriproject@gmail.com>
2026-04-20 10:24:32 +0300

    natap/natap.ino

--- bushiri_v33/bushiri_v33.ino
+++ bushiri_v33/bushiri_v33.ino
@@ -1,103 +1,25 @@
-/*
-  PROJECT BUSHIRI v3.3
-  MPESA/MIXX Captive Portal + NAT Router + VPS Verify + WiFi Repeater
-  Bei: TZS 800 = Masaa 15
-*/
-
 #include <WiFi.h>
-#include <WebServer.h>
-#include <HTTPClient.h>
-#include <DNSServer.h>
-#include <Update.h>
+#include "lwip/napt.h"
 
-const char* AP_SSID      = "Bushiri WiFi";
-const char* AP_PASS      = "";
-const char* VPS_HOST     = "bushiri-project.onrender.com";
-const int   VPS_PORT     = 443;
-const char* VPS_TOKEN    = "bushiri2026";
-const char* PORTAL_TITLE = "BUSHIRI HOTSPOT";
-const char* MIXX_NUMBER  = "0717633805";
-const char* STA_SSID_ALT = "PATAHUDUMA";
-const char* STA_PASS_ALT = ".AMUDUH123";
-
-String ownerMAC = "bc:90:63:a2:32:83"; // weka MAC ya simu yako bure
-String freeTrialMAC = "";
-unsigned long freeTrialStart = 0;
-
-DNSServer dnsServer;
-WebServer server(80);
-
-bool isPaidClient(String mac) {
-  if (mac == ownerMAC) return true; // mwenyewe bure
-  HTTPClient http;
-  String url = String("https://") + VPS_HOST + "/verify?mac=" + mac + "&token=" + VPS_TOKEN;
-  http.begin(url);
-  int httpCode = http.GET();
-  if (httpCode == 200) {
-    String payload = http.getString();
-    if (payload.indexOf("ALLOW") >= 0) return true;
-  }
-  http.end();
-  return false;
-}
-
-void handleRoot() {
-  String mac = WiFi.macAddress();
-  if (isPaidClient(mac)) {
-    server.send(200, "text/html", String("<h1>Karibu ") + PORTAL_TITLE + String("</h1><p>Umeunganishwa na internet.</p>"));
-  } else {
-    String page = String("<html><head><title>") + PORTAL_TITLE + String("</title></head><body style='background:linear-gradient(to right,orange,yellow,green,blue);color:white;text-align:center;'>");
-    page += String("<h1>") + PORTAL_TITLE + String("</h1>");
-    page += String("<p>Bei: TZS 800 = Masaa 15</p>");
-    page += String("<p>Tuma pesa kwa namba: ") + String(MIXX_NUMBER) + String("</p>");
-    page += String("<p>Utapokea TXID, weka hapa ili kuunganishwa.</p>");
-    page += String("<form action='/login'><input name='txid'><input type='submit' value='Login'></form>");
-    page += String("<h3>Free Trial (dakika 2)</h3>");
-    page += String("<form action='/trial'><input name='phone'><input type='submit' value='Jaribu'></form>");
-    page += String("</body></html>");
-    server.send(200, "text/html", page);
-  }
-}
-
-void handleLogin() {
-  String txid = server.arg("txid");
-  String mac = WiFi.macAddress();
-  HTTPClient http;
-  String url = String("https://") + VPS_HOST + "/login?txid=" + txid + "&mac=" + mac + "&token=" + VPS_TOKEN;
-  http.begin(url);
-  int httpCode = http.GET();
-  if (httpCode == 200) {
-    server.send(200, "text/html", String("<h1>Umeidhinishwa! Karibu internet.</h1>"));
-  } else {
-    server.send(200, "text/html", String("<h1>TXID haijakubalika.</h1>"));
-  }
-  http.end();
-}
-
-void handleTrial() {
-  String mac = WiFi.macAddress();
-  if (freeTrialMAC == mac && millis() - freeTrialStart < 86400000) {
-    server.send(200, "text/html", String("<h1>Samahani, free trial inapatikana tena baada ya masaa 24.</h1>"));
-    return;
-  }
-  freeTrialMAC = mac;
-  freeTrialStart = millis();
-  server.send(200, "text/html", String("<h1>Free trial imeanza, dakika 2 pekee.</h1>"));
-}
+const char* STA_SSID = "PATAHUDUMA";
+const char* STA_PASS = "HUDUMA";
+const char* AP_SSID  = "Bushiri WiFi";
+const char* AP_PASS  = "";
 
 void setup() {
+  Serial.begin(115200);
   WiFi.mode(WIFI_AP_STA);
+  WiFi.begin(STA_SSID, STA_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("Connected to modem");
+
   WiFi.softAP(AP_SSID, AP_PASS);
-  WiFi.begin(STA_SSID_ALT, STA_PASS_ALT);
+  delay(1000);
 
-  dnsServer.start(53, "*", WiFi.softAPIP());
-  server.on("/", handleRoot);
-  server.on("/login", handleLogin);
-  server.on("/trial", handleTrial);
-  server.begin();
+  ip_napt_enable(WiFi.softAPIP(), 1); // NAT enabled
 }
 
-void loop() {
-  dnsServer.processNextRequest();
-  server.handleClient();
-}
+void loop() {}
